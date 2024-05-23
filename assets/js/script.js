b5e5ffc9fe84f5a5b0c773bcdc9bc09de7e35f42
Karan Barale <karanbarale7047@gmail.com>
2024-05-23 16:35:54 +0530

    Project

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+
+menuIcon.onclick = function()
+{
+    sidebar.classList.toggle("small-sidebar")
+    container.classList.toggle("large-container")
+}
